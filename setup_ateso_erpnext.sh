c39fe180f098f20ac95f4450c5e4a2b714dc6bf9
Raphael Fuchs <raphael.fuchs@ateso.ch>
2023-01-17 18:54:47 +0100

    feat: extended setup script

--- setup_ateso_erpnext.sh
+++ setup_ateso_erpnext.sh
@@ -1,13 +1,35 @@
 # !/bin/bash
 
-docker-compose -f compose.yaml -f overrides/compose.noproxy.yaml -f overrides/compose.mariadb.yaml -f overrides/compose.redis.yaml -f compose.override.yaml up -d
-docker-compose exec backend bench new-site frontend --mariadb-root-password 123 --admin-password admin --install-app erpnext --install-app erpnextswiss --set-default
+# Check if Docker is installed
+if command -v docker >/dev/null 2>&1; then
+  echo "Docker is already installed."
+else
+  # Update package list and install Docker
+  sudo apt-get update
+  sudo apt-get install -y docker.io
+  echo "Docker has been installed."
+fi
+
+# Install ateso erpnext containers
+COMMAND="
+docker-compose -f compose.yaml -f overrides/compose.noproxy.yaml -f overrides/compose.mariadb.yaml -f overrides/compose.redis.yaml -f compose.override.yaml up -d;
+docker-compose exec backend bench new-site frontend --mariadb-root-password 123 --admin-password admin --install-app erpnext --install-app erpnextswiss --set-default;
 docker-compose restart backend
+"
 
-cat << EOF
+# Execute the command and store the return value
+$COMMAND
+RETURN_VALUE=$?
 
+# Check the return value
+if [ $RETURN_VALUE -eq 0 ]; then
+  echo "Installation executed successfully. You can reach the web interface via the https://your-system-ip:8080.
+  Username: administrator
+  Password: admin
 
-                               ^7JJ?~.                                
+  Enjoy!
+  
+                                 ^7JJ?~.                                
                              ^?JJJJJJ?~                               
                           .~?JJ?!^^~77: :.                            
                          ~?JJJ~       .7??!:                          
@@ -25,4 +47,7 @@ cat << EOF
 &@@.      !@@~   ^@@!   .@@B        ^^.  ~^.      Y@@7 .@@#       :@@B
 #@@7.  .^5@@@~   :@@G..  ~&@#!.  .~&@&.  #@@!.  .:G@@:  ^&@&~.  .J@@B 
  ?#@@@&&#YY@@^    J&@@@:   7B@@@@@@B7     7#@@@@@@&J.     7#@@@@@&G^  
-EOF
+  "
+else
+  echo "Installation failed with return value $RETURN_VALUE."
+fi
